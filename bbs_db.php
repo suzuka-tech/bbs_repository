4658a2dc33aff66d637fe8fb50470522077dbd97
Suzuka Funayama <suzuka-funayama@kalonade.co.jp>
2025-09-16 19:25:00 +0900

    port番号追加

--- bbs_db.php
+++ bbs_db.php
@@ -3,9 +3,10 @@ define('HOST','localhost');
 define('DB_NAME','bbs_db');
 define('USERNAME','root');
 define('PASSWORD','root');
+define('PORT','3307');
 
 try {
-    $pdo = new PDO("mysql:host=".HOST.";dbname=".DB_NAME,USERNAME,PASSWORD);
+    $pdo = new PDO("mysql:host=".HOST.";port=".PORT.";dbname=".DB_NAME,USERNAME,PASSWORD);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
     die("DB接続エラー: ".$e->getMessage());
